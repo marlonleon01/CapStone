4dcf7a5d0c6a508b7775df95d5ff08cc7c012187
Marlon Leon <ghostveene@gmail.com>
2023-02-22 22:58:05 -0500

    fetching author from api and displaying

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const authorOfImg = document.getElementById("author-of-img")
+
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(res => res.json())
     .then(data => {
         console.log(data)
+        authorOfImg.textContent = data.user.name
         document.body.style.backgroundImage= `url(${data.urls.full})`
     })
