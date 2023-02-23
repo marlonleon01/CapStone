3d61ca78c78a97350e0c15bfc8ba69ef71091a0d
Marlon Leon <ghostveene@gmail.com>
2023-02-22 23:37:51 -0500

    setup catch for img

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     .then(res => res.json())
     .then(data => {
         console.log(data)
-        authorOfImg.textContent = data.user.name
+        authorOfImg.textContent = `By: ${data.user.name}`
         document.body.style.backgroundImage= `url(${data.urls.full})`
     })
+    .catch(err => {
+        authorOfImg.textContent = `By: Jeremy Bishop`
+        document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1494564605686-2e931f77a8e2?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzcxMjY5NjY&ixlib=rb-4.0.3&q=80')"
+    })
+    
