9c4d2954fcb1a0b9ef42f7b3f76a422e362c7d16
Marlon Leon <ghostveene@gmail.com>
2023-02-23 00:45:37 -0500

    put fetch requests in function to seperate

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const authorOfImg = document.getElementById("author-of-img")
-const coinGeckoBaseUrl = "https://api.coingecko.com/api/v3"
+function getBackground() {
+    const authorOfImg = document.getElementById("author-of-img")
 
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(res => res.json())
     .then(data => {
         authorOfImg.textContent = `By: ${data.user.name}`
@@ -11,8 +11,13 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         authorOfImg.textContent = `By: Jeremy Bishop`
         document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1494564605686-2e931f77a8e2?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzcxMjY5NjY&ixlib=rb-4.0.3&q=80')"
     })
-    
-fetch(`${coinGeckoBaseUrl}/coins/bitcoin`)
+}
+getBackground()
+
+function getCoinInfo() {
+    const coinGeckoBaseUrl = "https://api.coingecko.com/api/v3"
+
+    fetch(`${coinGeckoBaseUrl}/coins/bitcoin`)
     .then(res => {
         if (!res.ok) {
             throw Error("Something went wrong")
@@ -33,11 +38,12 @@ fetch(`${coinGeckoBaseUrl}/coins/bitcoin`)
             `
     })
     .catch(err => alert(err))
+}
+getCoinInfo()
 
 function updateTime() {
     const date = new Date()
     const time = document.getElementById("time")
-    time.innerHTML = date.toLocaleTimeString("en-us", {timeStyle: "short"})
+    time.textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"})
 }
-
 setInterval(updateTime, 1000)
